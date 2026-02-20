615df2d46de847450c0e5958619d0f9bb82a3ec2
FxcuEsq <facundoesquivel03@gmail.com>
2026-02-19 22:31:53 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,50 +1,28 @@
-class Remera {
-    constructor(nombre, precio, imagen) {
-        this.nombre = nombre;
-        this.precio = precio;
-        this.imagen = imagen;
-    }
-
-    render() {
-        return `
-            <div class="card">
-                <img src="${this.imagen}" alt="${this.nombre}">
-                <h2>${this.nombre}</h2>
-                <p>$${this.precio}</p>
-            </div>
-        `;
-    }
-}
+import Remera from "./Remera.js";
 
 const contenedor = document.getElementById("contenedor");
 
 const remeras = [
-    new Remera("Oversize", 20000, "img/berserk_front.jpeg"),
-    new Remera("Oversize", 20000, "img/berserk_back.jpeg"),
-    new Remera("Oversize", 20000, "img/one_piece_front.jpeg"),
-    new Remera("Oversize", 20000, "img/one_piece_back.jpeg"),
-    new Remera("Oversize", 20000, "img/toji_front.jpeg"),
-    new Remera("Oversize", 20000, "img/toji_back.jpeg"),
-    new Remera("Remera Clasica Mujer", 15000, "img/butterfly.jpeg"),
-    new Remera("Boxy Fit", 18000, "img/fentanxl.jpeg"),
-    new Remera("Musculosa Mujer", 10000, "img/gojo_eye.jpeg"),
-    new Remera("Musculosa Mujer", 10000, "img/heart_red.jpeg")
+    new Remera("Urban Violeta", 15000, "img/remera1.jpg", {
+        S: 5,
+        M: 3,
+        L: 2,
+        XL: 0
+    }),
+    new Remera("Minimal White", 14000, "img/remera2.jpg", {
+        S: 4,
+        M: 6,
+        L: 1,
+        XL: 2
+    }),
+    new Remera("Grey Classic", 16000, "img/remera3.jpg", {
+        S: 2,
+        M: 0,
+        L: 3,
+        XL: 1
+    })
 ];
-
 
 remeras.forEach(remera => {
     contenedor.innerHTML += remera.render();
-
-
-const form = document.getElementById("formIdeas");
-const mensaje = document.getElementById("mensaje");
-
-form.addEventListener("submit", function(e) {
-    e.preventDefault();
-
-    mensaje.innerHTML = "Gracias por tu aporte <3.";
-
-    form.reset();
-});
-
 });
