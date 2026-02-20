7e0971b8936c520b561646be7eade2d327685c8e
FxcuEsq <facundoesquivel03@gmail.com>
2026-02-19 22:31:00 -0300

    Update Remera.js

--- js/Remera.js
+++ js/Remera.js
@@ -1,17 +1,28 @@
-export default class Remera {
-    constructor(nombre, precio, imagen) {
-        this.nombre = nombre;
-        this.precio = precio;
-        this.imagen = imagen;
-    }
+import Remera from "./Remera.js";
 
-    render() {
-        return `
-            <div class="card">
-                <img src="${this.imagen}" alt="${this.nombre}">
-                <h2>${this.nombre}</h2>
-                <p>$${this.precio}</p>
-            </div>
-        `;
-    }
-}
+const contenedor = document.getElementById("contenedor");
+
+const remeras = [
+    new Remera("Urban Violeta", 15000, "img/remera1.jpg", {
+        S: 5,
+        M: 3,
+        L: 2,
+        XL: 0
+    }),
+    new Remera("Minimal White", 14000, "img/remera2.jpg", {
+        S: 4,
+        M: 6,
+        L: 1,
+        XL: 2
+    }),
+    new Remera("Grey Classic", 16000, "img/remera3.jpg", {
+        S: 2,
+        M: 0,
+        L: 3,
+        XL: 1
+    })
+];
+
+remeras.forEach(remera => {
+    contenedor.innerHTML += remera.render();
+});
